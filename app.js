25bec7793a6a487380dc33006f058b8c6c1bbba0
Wes Reid <bwreid@gmail.com>
2019-05-28 18:47:03 -0700

    Add morgan, nodemon, general middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const { NODE_ENV='development', PORT=5000 } = process.env
 const express = require('express')
 const app = express()
-const port = 5000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
-app.listen(port, listener)
+if (NODE_ENV === 'development') app.use(require('morgan')('dev'))
+
+app.use((req, res, next) => {
+  console.log('You are inside the server!')
+  next()
+})
+
+app.use((req, res) => {
+  res.status(200).json({
+    message: `You made it!`
+  })
+})
+
+const listener = () => console.log(`Application listening on port ${PORT}!`)
+app.listen(PORT, listener)

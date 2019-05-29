1fef415e18e8698fc8349eb4a500a5de786ad9fd
Wes Reid <bwreid@gmail.com>
2019-05-28 18:35:32 -0700

    Update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 const listener = () => console.log(`Application listening on port ${port}!`)
 app.listen(port, listener)
